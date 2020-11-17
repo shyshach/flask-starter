b7a468137e4ad00450b0e1241da9100deebb07ee
Rostyslav Shymchak <rostyslav.shymchak@jetsoftpro.com>
2020-11-17 12:03:46 +0200

    Add basic JWT auth

--- src/models/user.py
+++ src/models/user.py
@@ -1,6 +1,6 @@
 from . import db
 from .abc import BaseModel
-
+from passlib.hash import pbkdf2_sha256 as sha256
 import datetime
 
 
@@ -10,7 +10,17 @@ class User(db.Model, BaseModel):
     )
     avatar_url = db.Column(db.String, nullable=True)
     date_created = db.Column(db.DateTime, default=datetime.datetime.utcnow)
+    password = db.Column(db.String, nullable=True)
 
-    def __init__(self, username: str, avatar_url: str = ''):
+    def __init__(self, username: str, password: str, avatar_url: str = ''):
         self.username = username
         self.avatar_url = avatar_url
+        self.password = password
+
+    @staticmethod
+    def generate_hash(password):
+        return sha256.hash(password)
+
+    @staticmethod
+    def verify_hash(password, hash):
+        return sha256.verify(password, hash)
